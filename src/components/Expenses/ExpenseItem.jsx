150a5967955120b3f680de73a3ffd8952c9a28fc
perisicnikola37 <perisicnikola37@gmail.com>
2023-04-09 20:18:28 +0200

    Fix code

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -4,7 +4,7 @@ import Card from "../UI/Card.jsx";
 import ExpenseDate from "./ExpenseDate.jsx";
 
 const ExpenseItem = (props) => {
-  const [tittle, setTittle] = useState(props.tittle);
+  const [tittle, setTittle] = useState(props.title);
 
   const clickHandler = () => {
     setTittle("Updated!");
@@ -12,13 +12,13 @@ const ExpenseItem = (props) => {
 
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{tittle}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Tittle</button>
-    </Card>   
+    </Card>
   );
 }
 
